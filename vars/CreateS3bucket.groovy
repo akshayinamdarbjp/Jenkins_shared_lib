01705106652ada1ac30764bf45b474356d8ef0a5
Akshay Inamdar <akshay.inamdar@microfocus.com>
2021-07-28 00:27:32 +0530

    changed file

--- vars/CreateS3bucket.groovy
+++ vars/CreateS3bucket.groovy
@@ -1,2 +1,3 @@
-def call(Map params)
-	powershell(script: " aws s3api create-bucket --bucket params.BucketName --region params.region --create-bucket-configuration LocationConstraint=us-west-2")
+def call(Map params) {
+	powershell(script: " aws s3api create-bucket --bucket params.BucketName --region params.region --create-bucket-configuration LocationConstraint=us-west-2")		
+	}
